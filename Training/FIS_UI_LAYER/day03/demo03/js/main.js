7a799422bfce66d2d6db04992eaa407c7d804ec0
Dhara-7615 <89508950+Dhara-7615@users.noreply.github.com>
2021-09-02 10:35:11 +0530

    Add files via upload

---
+++ Training/FIS_UI_LAYER/day03/demo03/js/main.js
@@ -0,0 +1,11 @@
+function add(){
+var v1 = document.getElementById("n1").value;
+var v2 = document.getElementById("n2").value;
+console.log(v1)
+console.log(v2)
+var result = 0 ;
+result = parseInt(v1)+parseInt(v2);
+console.log(result);
+alert("sum is : "+result);
+}
+
